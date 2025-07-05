710433124a826048039f130a3ecd3e2124288068
ANCHAL <anchal2004tandekar@gmail.com>
2025-07-05 23:57:08 +0530

    Refactor vector embedding script to fetch URLs dynamically and update query for improved relevance

--- vectorEmbeding.py
+++ vectorEmbeding.py
@@ -3,18 +3,18 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
 
+from urlCrawl import extractFromUrl
+from searchResult import getSearchUrls
 load_dotenv()
 
-raw_content = [
-    "Stripe Payments is a global payment processing platform.",
-    "Another document about refactoring in Python."
-]
+urls = getSearchUrls("How to bake a cake", 5)
+raw_content = extractFromUrl(urls)
 
 embaddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 vectorDb = Chroma.from_texts(raw_content, embaddings, persist_directory='./chromaDb')
 
-query = "What is gay"
-results = vectorDb.similarity_search(query, k=2)
+query = "at what temprature to bake a cake"
+results = vectorDb.similarity_search(query, k=1)
 for doc in results:
     print(doc.page_content)
