8fd787bf62ae36e3b1d317b7d9623b7d6bd33831
ANCHAL <anchal2004tandekar@gmail.com>
2025-07-05 22:47:58 +0530

    Update .gitignore to include __pycache__ and modify URL fetching in urlCrawl.py for improved search results

--- urlCrawl.py
+++ urlCrawl.py
@@ -7,10 +7,13 @@ from searchResult import getSearchUrls
 load_dotenv()
 
 query = "What are the best ways to refactor in python"
-urls = getSearchUrls(query, 5)
+urls = getSearchUrls(query, 1)
 
 client = TavilyClient(os.getenv("TAVILY_API_KEY"))
 response = client.extract(
     urls=urls
 )
-print(response)
+
+raw_content = [item.get('raw_content', '') for item in response.get("results", [])]
+
+print(raw_content)
