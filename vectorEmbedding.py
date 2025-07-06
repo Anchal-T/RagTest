91f622b5c6ce92da6044a7159d90e220e71708e7
ANCHAL <anchal2004tandekar@gmail.com>
2025-07-06 13:23:30 +0530

    Refactor vectorEmbedding.py to encapsulate database building logic in buildVecDb function for improved modularity

--- vectorEmbedding.py
+++ vectorEmbedding.py
@@ -7,14 +7,13 @@ from urlCrawl import extractFromUrl
 from searchResult import getSearchUrls
 load_dotenv()
 
-urls = getSearchUrls("How to bake a cake", 5)
-raw_content = extractFromUrl(urls)
+def buildVecDb(query, urlNums=5):
+    urls = getSearchUrls(query, urlNums)
+    raw_content = extractFromUrl(urls)
+    embaddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    Chroma.from_texts(raw_content, embaddings, persist_directory='./chromaDb')
+    print('Vector Db built')
 
-embaddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-vectorDb = Chroma.from_texts(raw_content, embaddings, persist_directory='./chromaDb')
-
-query = "at what temprature to bake a cake"
-results = vectorDb.similarity_search(query, k=1)
-for doc in results:
-    print(doc.page_content)
+if __name__ == "__main__":
+    query = "How to bake a cake"
+    buildVecDb(query, 5)
