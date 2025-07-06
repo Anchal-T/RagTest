086d2bb4a490e4ceca556df3b2cd4d51f5fa0226
ANCHAL <anchal2004tandekar@gmail.com>
2025-07-06 13:47:27 +0530

    Refactor buildVecDb function to split text into chunks before creating embeddings for improved processing

--- vectorEmbedding.py
+++ vectorEmbedding.py
@@ -1,3 +1,7 @@
+from transformers.masking_utils import chunked_overlay
+from h11._abnf import chunk_size
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
+from langchain_text_splitters.base import TextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
@@ -10,8 +14,15 @@ load_dotenv()
 def buildVecDb(query, urlNums=5):
     urls = getSearchUrls(query, urlNums)
     raw_content = extractFromUrl(urls)
-    embaddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    Chroma.from_texts(raw_content, embaddings, persist_directory='./chromaDb')
+
+    textSplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+
+    allChunks = []
+    for doc in raw_content:
+        allChunks.extend(textSplitter.split_text(doc))
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    Chroma.from_texts(allChunks, embeddings, persist_directory='./chromaDb')
     print('Vector Db built')
 
 if __name__ == "__main__":
