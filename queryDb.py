8bec09139e95ed64f6809f62a42fa3c46605de04
ANCHAL <anchal2004tandekar@gmail.com>
2025-07-06 13:28:35 +0530

    Add query_vector_db function to perform similarity search using HuggingFace embeddings and Chroma

---
+++ queryDb.py
@@ -0,0 +1,13 @@
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def query_vector_db(query, k=1):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorDb = Chroma(persist_directory='./chromaDb', embedding_function=embeddings)
+    results = vectorDb.similarity_search(query, k=k)
+    for doc in results:
+        print(doc.page_content)
+
+if __name__ == "__main__":
+    query = "at what temperature to bake a cake"
+    query_vector_db(query, k=1)
